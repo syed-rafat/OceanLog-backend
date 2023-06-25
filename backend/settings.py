ba22c06691a62246a99340a518ba7547d7abad24
Syed Rafat <hoqrafat@gmail.com>
2023-06-25 22:34:15 +0600

    Vercel postgresql db added

--- backend/settings.py
+++ backend/settings.py
@@ -113,10 +113,10 @@ if local==False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get("DB_NAME"),
-            'HOST': os.environ.get("DB_HOST"),
-            'USER': os.environ.get("DB_USER"),
-            'PASSWORD': os.environ.get("DB_PASSWORD"),
+            'NAME': os.environ.get("POSTGRES_DATABASE"),
+            'HOST': os.environ.get("POSTGRES_HOST"),
+            'USER': os.environ.get("POSTGRES_USER"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
             'PORT': os.environ.get("DB_PORT"),
         }
     }
